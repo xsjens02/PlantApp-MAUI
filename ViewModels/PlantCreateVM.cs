aad1e8e494b668a4882d0d3e1b4a8007be4b0427
Kaqnae <noahvi01@easv365.dk>
2025-04-04 08:37:37 +0200

    TakePhoto implemented

--- ViewModels/PlantCreateVM.cs
+++ ViewModels/PlantCreateVM.cs
@@ -3,38 +3,62 @@ using CommunityToolkit.Mvvm.Input;
 using Microsoft.Maui.Storage;
 using System.Diagnostics;
 
-namespace PlantAppMAUI.ViewModels
+namespace PlantAppMAUI.ViewModels;
+
+public partial class PlantCreateVM : ObservableObject
 {
-    public partial class PlantCreateVM : ObservableObject
+    public PlantCreateVM()
     {
-        public PlantCreateVM()
-        {
-            
-        }
 
-        [RelayCommand]
-        private async Task GoToHome() => await Shell.Current.GoToAsync("//HomePage");
-        [RelayCommand]
-        private async Task PickImageFile()
+    }
+
+    [RelayCommand]
+    private async Task GoToHome() => await Shell.Current.GoToAsync("//HomePage");
+    [RelayCommand]
+    private async Task PickImageFile()
+    {
+        try
         {
-            try
+            var selectedFile = await FilePicker.Default.PickAsync(new PickOptions
             {
-                var selectedFile = await FilePicker.Default.PickAsync(new PickOptions
-                {
-                    PickerTitle = "Choose a file",
-                    FileTypes = FilePickerFileType.Images,
-                });
+                PickerTitle = "Choose a file",
+                FileTypes = FilePickerFileType.Images,
+            });
 
-                if (selectedFile != null)
-                {
-                    var filePath = selectedFile.FullPath;
-                    Debug.WriteLine($"Choosen file: {filePath}");
-                }
+            if (selectedFile != null)
+            {
+                var filePath = selectedFile.FullPath;
+                Debug.WriteLine($"Choosen file: {filePath}");
             }
-            catch (Exception e)
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"Error: {e}");
+        }
+    }
+
+    [RelayCommand]
+    private async Task TakePhoto()
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+            if (photo != null)
             {
-                Debug.WriteLine($"Error: {e}");
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenRead(localFilePath);
+
+                await sourceStream.CopyToAsync(localFileStream);
             }
         }
     }
-}
+}   
+            
+        
+
+    
+
+
